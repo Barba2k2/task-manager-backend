26a8e5e992ee33a02253c5e1b1da01da6456b281
Barba2k2 <lorenzo.dz@hotmail.com>
2024-02-07 10:53:56 -0300

    feat: add route to create a new tasks

--- index.js
+++ index.js
@@ -2,15 +2,35 @@ const express = require("express");
 const dotenv = require("dotenv");
 
 const connectToDatabse = require("./src/databse/mongoose.database");
+const TaskModel = require("./src/models/task.model");
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 connectToDatabse();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello, world!");
+app.get("/tasks", async (req, res) => {
+  try {
+    const tasks = await TaskModel.find({});
+    res.status(200).send(tasks);
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.error(error);
+  }
+});
+
+app.post("/tasks", async (req, res) => {
+  try {
+    const newTask = new TaskModel(req.body);
+
+    await newTask.save();
+    res.status(201).send(newTask);
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.error(error);
+  }
 });
 
 app.listen(8000, () => console.log("Listening on port 8000!"));
